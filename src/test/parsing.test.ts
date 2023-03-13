bda381d88948616c88a8532669d7645a3678569e
Theodor Tonum <theodor@tonum.no>
2023-03-13 10:45:19 +0100

    Suffix raw numbers with px

--- src/test/parsing.test.ts
+++ src/test/parsing.test.ts
@@ -20,3 +20,18 @@ test("simple transformation should work", () => {
   expect(styleAttr?.properties[0].key).toBe("marginTop");
   expect(styleAttr?.properties[0].value).toBe("12px");
 });
+
+test("raw numbers are suffixed with px", () => {
+  const t = `
+        <div style={{
+            marginTop: 12
+        }}/>
+    `;
+  const { selectedElement, styleAttr } = parseDocument(t, 10);
+
+  expect(selectedElement).toBeDefined();
+  expect(styleAttr).toBeDefined();
+  expect(styleAttr?.properties).toHaveLength(1);
+  expect(styleAttr?.properties[0].key).toBe("marginTop");
+  expect(styleAttr?.properties[0].value).toBe("12px");
+});
